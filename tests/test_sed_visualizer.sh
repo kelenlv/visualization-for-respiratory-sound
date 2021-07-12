5572cdff4f49d57cdb25f8bbf6499232f2a9634c
kelenlv <Kelen_Lv@sjtu.edu.cn>
2021-07-12 10:15:03 +0800

    Update test_sed_visualizer.sh

--- tests/test_sed_visualizer.sh
+++ tests/test_sed_visualizer.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-../visualizers/sed_visualizer.py -a ../tests/data/a001.wav -l ../tests/data/a001.ann ../tests/data/a001_full.ann ../tests/data/a001_system_output_prob.csv -n reference1 full reference2 -e "bird singing" "car passing by" --publication
+./sed_visualizer_lkx.py -a ../tests/130_1p2_Al_mc_AKGC417L.wav -l ../tests/130_1p2_Al_mc_AKGC417L.txt -n reference
